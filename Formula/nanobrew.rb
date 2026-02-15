5bab09bacfde045a91a78a28ce767dcdec10b1d9
Rach Pradhan <54503978+justrach@users.noreply.github.com>
2026-02-15 22:50:03 +0800

    fix: remove duplicate license line in formula

--- Formula/nanobrew.rb
+++ Formula/nanobrew.rb
@@ -1,9 +1,9 @@
 class Nanobrew < Formula
   desc "The fastest macOS package manager. Written in Zig."
   homepage "https://github.com/justrach/nanobrew"
-  license "MIT"
-  version "0.1.0"
   license "Apache-2.0"
+  version "0.1.0"
+
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/justrach/nanobrew/releases/download/v0.1.0/nb-arm64-apple-darwin.tar.gz"
